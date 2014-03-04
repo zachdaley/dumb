1792f6f80cad906bbecb43407ac487f858f84189
Daley <zdaley@engr-131-02.eastern.ewu.edu>
2014-03-04 14:51:43 -0800

    Linking weather.js

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,24 @@ $(document).foundation();
 // Your Awesome Scripts!
 $(document).ready(function(){
 
-	console.log('Page Loaded. Lets Do this!');
+	$(document).ready(function() {
+  $.simpleWeather({
+    location: 'Austin, TX',
+    woeid: '',
+    unit: 'f',
+    success: function(weather) {
+      html = '<h2><i class="icon-'+weather.code+'"></i> '+weather.temp+'&deg;'+weather.units.temp+'</h2>';
+      html += '<ul><li>'+weather.city+', '+weather.region+'</li>';
+      html += '<li class="currently">'+weather.currently+'</li>';
+      html += '<li>'+weather.wind.direction+' '+weather.wind.speed+' '+weather.units.speed+'</li></ul>';
+  
+      $("#weather").html(html);
+    },
+    error: function(error) {
+      $("#weather").html('<p>'+error+'</p>');
+    }
+  });
+});
+
 
 }); 
